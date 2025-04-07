60a17b690a89157d414f3456144bc782899bc71c
Jaad <jaadouaich@hotmail.be>
2025-04-07 16:50:56 +0200

    un debut de nav bar vertic

--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -1,7 +1,7 @@
+import React from "react";
 import { Link } from '@inertiajs/react';
 
 export default function NavLink({
-    active = false,
     className = '',
     children,
     ...props
@@ -11,8 +11,8 @@ export default function NavLink({
             {...props}
             className={
                 'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none no-underline ' +
-                (active
-                    ? 'text-white'  
+                (props.href === window.location.pathname 
+                    ? 'text-white' 
                     : 'text-gray-500 hover:text-gray-700 hover:underline focus:text-gray-700') + className
             }
         >
--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -4,17 +4,19 @@ import NavLink from '@/Components/NavLink';
 import ResponsiveNavLink from '@/Components/ResponsiveNavLink';
 import { Link, usePage } from '@inertiajs/react';
 import { useState } from 'react';
+import NavBarVertical from '@/Components/NavBarVertical';
 
 export default function AuthenticatedLayout({ header, children }) {
     const user = usePage().props.auth.user;
-
-    const [showingNavigationDropdown, setShowingNavigationDropdown] =
-        useState(false);
+    const [showingNavigationDropdown, setShowingNavigationDropdown] = useState(false);
 
     return (
-        <div className="min-h-screen bg-dark-primary">
+        <div className="min-h-screen bg-dark-primary flex">
+            <NavBarVertical />
 
-            <main>{children}</main>
+            <div className="flex-1 p-6">
+                <main>{children}</main>
+            </div>
         </div>
     );
 }
