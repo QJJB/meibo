2e769f27e11b5af2d33c8a192a1b700b9238745e
Jaad <jaadouaich@hotmail.be>
2025-04-07 15:49:06 +0200

    test nav-links dashboard

---
+++ resources/js/Components/NavBarVertical.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function NavBarVertical() {
+  return (
+    <div>NavBarVertical</div>
+  )
+}
+
+export default NavBarVertical
--- resources/js/Components/NavLink.jsx
+++ resources/js/Components/NavLink.jsx
@@ -10,10 +10,10 @@ export default function NavLink({
         <Link
             {...props}
             className={
-                'inline-flex items-center border-b-2 px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none ' +
+                'inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 transition duration-150 ease-in-out focus:outline-none no-underline ' +
                 (active
-                    ? 'border-indigo-400 text-gray-900 focus:border-indigo-700'
-                    : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700 focus:border-gray-300 focus:text-gray-700') +
+                    ? 'text-white'  // lien actif sans bordure ni soulignement
+                    : 'text-gray-500 hover:text-gray-700 hover:underline focus:text-gray-700') +  // soulignement au hover pour les liens inactifs
                 className
             }
         >
