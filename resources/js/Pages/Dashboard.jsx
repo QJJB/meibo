dd1d774263bb361026f77520828197bf1441900d
Ugo <j.erghioui@gmail.com>
2025-04-13 07:46:42 +0200

    Up Tailwind 3 -> 4, Delete PostCSS, Migration Theme -> app.css

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -6,7 +6,7 @@ export default function Dashboard() {
         <AuthenticatedLayout>
             <Head title="Dashboard" />
 
-            <div className="mt-[15px] mb-[15px] mr-[15px] bg-dark-secondary rounded-[20px] shadow-sm overflow-hidden w-[calc(100%-15px)] h-[calc(100vh-30px)]">
+            <div className="mt-[15px] mb-[15px] mr-[15px] bg-dark-secondary rounded-[20px] shadow-xs overflow-hidden w-[calc(100%-15px)] h-[calc(100vh-30px)]">
     <div className="p-6 text-white">
         Saleeeeeee 
     </div>
