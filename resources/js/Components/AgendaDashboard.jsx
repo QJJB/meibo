8673ebebd86ba8e4405166f5e548289eb64d8644
Jaad <jaadouaich@hotmail.be>
2025-04-17 16:04:12 +0200

    task card creation and tasksboard

--- resources/js/Components/AgendaDashboard.jsx
+++ resources/js/Components/AgendaDashboard.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const AgendaDashboard = () => {
   return (
     <div className='bg-dark-secondary rounded-[20px]'>
-      <h1 className='text-[30px] font-semibold'>Agenda</h1>
+      <h1 className='text-white-title mt-[25px] ml-[30px] text-[30px] leading-[30px] font-semibold'>Agenda</h1>
     </div>
   )
 }
--- resources/js/Components/TasksDashboard.jsx
+++ resources/js/Components/TasksDashboard.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import TaskCard from './TaskCard'
 
 function TasksDashboard() {
   return (
-    <div className='bg-dark-secondary rounded-[20px]'></div>
+    <div className='bg-dark-secondary rounded-[20px]'>
+      <div className="header-tasks-dashboard flex justify-between items-center mt-[30px] ml-[30px] w-[256px]">
+        <h2 className='text-white-title text-[30px] leading-[30px] font-semibold [letter-spacing:-0.05em]'>Tasks</h2>
+        <div className="header-separation-tasks w-[3px] h-[33px] bg-header-separation round-[20px]"></div>
+        <p className='text-gray-title-secondary text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em]'>Go to Archives</p>  
+      </div>
+      <div className="flex flex-col task-cards mt-[35px] space-y-[20px]">
+        <TaskCard></TaskCard>
+        <TaskCard></TaskCard>
+      </div> 
+    </div>
   )
 }
 
