f5d1e784fe58ac2d1a1d13e8a276c7023de8856b
Jaad <jaadouaich@hotmail.be>
2025-04-22 10:56:38 +0200

    scroll in tasks section

--- resources/js/Components/TasksDashboard.jsx
+++ resources/js/Components/TasksDashboard.jsx
@@ -1,20 +1,33 @@
-import React from 'react'
-import TaskCard from './TaskCard'
+import React from 'react';
+import TaskCard from './TaskCard';
 
 function TasksDashboard() {
   return (
-    <div className='bg-dark-secondary rounded-[20px]'>
-      <div className="header-tasks-dashboard flex justify-between items-center mt-[30px] ml-[30px] w-[256px]">
-        <h2 className='text-white-title text-[30px] leading-[30px] font-semibold [letter-spacing:-0.05em]'>Tasks</h2>
-        <div className="header-separation-tasks w-[3px] h-[33px] bg-header-separation round-[20px]"></div>
-        <p className='text-gray-title-secondary text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em]'>Go to Archives</p>  
+    <div className="bg-dark-secondary rounded-[20px] overflow-auto scrollbar-hide max-h-[600px] relative">
+      {/* Sticky header that sticks at its original position */}
+      <div className="h-[100px] bg-dark-secondary z-0 sticky top-0">
+        <div className="header-tasks-dashboard sticky top-[30px] z-10 bg-dark-secondary flex justify-between items-center ml-[30px] w-[256px] pb-[10px]">
+          <h2 className='text-white-title text-[30px] leading-[30px] font-semibold [letter-spacing:-0.05em]'>
+          Tasks
+          </h2>
+        <div className="header-separation-tasks w-[3px] h-[33px] bg-header-separation rounded-[20px]"></div>
+          <p className='text-gray-title-secondary text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em]'>
+          Go to Archives
+          </p>  
+        </div>
       </div>
-      <div className="flex flex-col task-cards mt-[35px] space-y-[20px]">
-        <TaskCard></TaskCard>
-        <TaskCard></TaskCard>
-      </div> 
+      
+
+      {/* Scrollable content starts after header */}
+      <div className="flex flex-col task-cards mt-[35px] space-y-[20px] overflow-hidden pb-[30px]">
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+        <TaskCard />
+      </div>
     </div>
-  )
+  );
 }
 
-export default TasksDashboard
+export default TasksDashboard;
