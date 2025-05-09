2e43d40b608810c8182a214bc0fcfd0a3915193d
Ugo <j.erghioui@gmail.com>
2025-05-09 14:06:08 +0200

    wip

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,31 +1,22 @@
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import AgendaDashboard from '@/Components/AgendaDashboard';
-import { Head } from '@inertiajs/react';
-import NewsDashboard from '@/Components/NewsDashboard';
 import ProjectsDashboard from '@/Components/ProjectsDashboard';
 import TasksDashboard from '@/Components/TasksDashboard';
-import TaskCard from "@/Components/TaskCard.jsx";
-import React from "react";
 import { usePage } from '@inertiajs/react';
 
+import { Head } from '@inertiajs/react';
+import NewsDashboard from '@/Components/NewsDashboard';
+import TaskCard from "@/Components/TaskCard.jsx";
+
 export default function Dashboard() {
-
-    const { tasks, projects, className } = usePage().props;
-
+    const { tasks, projects } = usePage().props;
     return (
-
         <AuthenticatedLayout>
             <div className="grid grid-cols-[1.6fr_1fr] grid-rows-[1fr_3fr] gap-[15px] h-[100vh] p-[15px] box-border">
-                {/* Ligne 1 */}
-                <AgendaDashboard tasks={tasks} projects={projects} />     {/* 1/4 de la hauteur = 1fr dans une grille 1fr/3fr */}
-                {/* <NewsDashboard className="h-[20%]" />       1/5 = 20% de 100vh */}
-
-                {/* Ligne 2 */}
-                <TasksDashboard tasks={tasks} />      {/* 4/5 = 80% */}
-                <ProjectsDashboard projects={projects} />    {/* 3/4 = 3fr */}
-                
+                <AgendaDashboard tasks={tasks} projects={projects} />
+                <TasksDashboard tasks={tasks} />
+                <ProjectsDashboard projects={projects} />              
             </div>
         </AuthenticatedLayout>
-
     );
 }
