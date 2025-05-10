428de2b2b1ece833380aa53a217bf43886ae27c6
Ugo <j.erghioui@gmail.com>
2025-05-10 14:30:07 +0200

    Agenda update - Fitler + Map :  Data

--- resources/js/Components/AgendaDashboard.jsx
+++ resources/js/Components/AgendaDashboard.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
+
 import ppKirby from "../../assets/pp/kirby.jpg";
+
 import sunSVG from "../../assets/sun.svg";
 import moreSVG from "../../assets/more.svg";
+
 import leftSVG from "../../assets/left.svg";
 import rightSVG from "../../assets/right.svg";
 
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 
-const AgendaDashboard = ({ tasks, projects }) => {
+const AgendaDashboard = ({ projects, auth }) => {
     const [activeDay, setActiveDay] = useState(dayjs());
-    const previousDay = activeDay.subtract(1, "day");
-    const nextDay = activeDay.add(1, "day");
-
-    const [leftHover, setLeftHover] = useState(false);
-    const [rightHover, setRightHover] = useState(false);
+    const previousDay = dayjs(activeDay).subtract(1, "day");
+    const nextDay = dayjs(activeDay).add(1, "day");
 
     function goLeft() {
         setActiveDay((prev) => prev.subtract(1, "day"));
@@ -25,36 +25,45 @@ const AgendaDashboard = ({ tasks, projects }) => {
     }
 
     dayjs.extend(customParseFormat);
-    const todayFormatted = dayjs().format("MMM D, YYYY");
 
-    function goLeft() {
-        setActiveDay((prev) => prev.subtract(1, "day"));
-    }
+    const todayFormatted = activeDay.format("MMM, YYYY");
 
-    function goRight() {
-        setActiveDay((prev) => prev.add(1, "day"));
-    }
+    const myTasks = projects.flatMap((project) =>
+        project.tasks
+            .filter((task) => task.created_by === auth.user.id)
+            .map((task) => ({
+                projectId: project.id,
+                projectName: project.name,
+                profilePicture: project.users.profile_picture_path,
+                dueDate: dayjs(task.due_date, "YYYY-MM-DD HH:mm:ss"),
+            }))
+    );
 
-    const deadLines = tasks.map(task => {
-      const project = projects.find(p => p.id === task.project_id);
-      return {
-        projectName: project?.name || null,
-        taskDueDate: task.due_date
-      };
-    });
-    
+    const tasksPreviousDay = myTasks.filter((task) =>
+        dayjs(task.dueDate).isSame(previousDay, "day")
+    );
 
-    tasks.map((task) => (
-      console.log("TASK", task)
-    ));
+    const showPreviousDay = tasksPreviousDay.slice(
+        0,
+        tasksPreviousDay.length > 3 ? 2 : 3
+    );
 
-    projects.map((project) => (
-      console.log("PROJECT", project)
-    ));
+    const tasksActiveDay = myTasks.filter((task) =>
+        dayjs(task.dueDate).isSame(activeDay, "day")
+    );
 
-    deadLines.map((deadLine) => (
-      console.log("DEADLINE", deadLine)
-    ));
+    const showActiveDay = tasksActiveDay.slice(
+        0,
+        tasksActiveDay.length > 3 ? 2 : 3
+    );
+
+    const tasksNextDay = myTasks.filter((task) =>
+        dayjs(task.dueDate).isSame(nextDay, "day")
+    );
+
+    const showNextDay = tasksNextDay.slice(0, tasksNextDay.length > 3 ? 2 : 3);
+
+    // console.log(JSON.stringify(myTasks, null, 2));
 
     return (
         <div className="agenda bg-dark-secondary rounded-[20px] px-[30px] py-[30px]">
@@ -68,7 +77,21 @@ const AgendaDashboard = ({ tasks, projects }) => {
                         {todayFormatted}
                     </p>
                 </div>
-                <div className="right ">
+                <div className="right flex">
+                    <div className="arrows flex gap-[8px] pr-[20px]">
+                        <div
+                            className="arrow-left flex h-[30px] w-[30px] m-auto justify-center items-center bg-dark-tertiary rounded-full hover:cursor-pointer"
+                            onClick={goLeft}
+                        >
+                            <img src={leftSVG} alt="Left arrow" />
+                        </div>
+                        <div
+                            className="arrow-right flex h-[30px] w-[30px] m-auto justify-center items-center bg-dark-tertiary rounded-full hover:cursor-pointer"
+                            onClick={goRight}
+                        >
+                            <img src={rightSVG} alt="Left arrow" />
+                        </div>
+                    </div>
                     <button
                         className={`today text-[20px] leading-[20px] [letter-spacing:-0.05em] font-[500] border-2 px-[25px] py-[5px] rounded-[20px] hover:cursor-pointer
                         ${
@@ -82,107 +105,162 @@ const AgendaDashboard = ({ tasks, projects }) => {
                     </button>
                 </div>
             </div>
-
             <div className="body flex justify-between h-full py-[30px]">
-                <div className="separator w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
+                <div className="separator min-w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
 
-                <div
-                    className="day prev w-full px-[10px] flex flex-col gap-[10px]"
-                    onMouseEnter={() => setLeftHover(true)}
-                    onMouseLeave={() => setLeftHover(false)}
-                >
-                    <h3 className="text-gray-title-secondary text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px]">
-                        {leftHover ? (
-                            <div
-                                className="left-arrow flex h-[23px] w-[23px] m-auto justify-center items-center bg-dark-tertiary rounded-full hover:cursor-pointer"
-                                onClick={goLeft}
-                            >
-                                <img src={leftSVG} alt="Left arrow" />
-                            </div>
-                        ) : (
-                            previousDay.date()
-                        )}
+                <div className="day prev w-full px-[10px] grid grid-rows-4 min-h-[200px]">
+                    <h3
+                        className={`text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px] ${
+                            previousDay.isSame(dayjs(), "day")
+                                ? "text-white-title"
+                                : "text-gray-title-secondary"
+                        }`}
+                    >
+                        {previousDay.date()}
                     </h3>
 
-                    <div className="task bg-yellow-meibo border-[2px] border-gray-title-secondary rounded-[15px] h-[45px] flex items-center justify-between px-[10px]">
-                        <div className="left flex items-center">
-                            <div className="w-7 h-7 rounded-full overflow-hidden mr-[10px]">
-                                <img
-                                    src={ppKirby}
-                                    alt="Avatar"
-                                    className="w-full h-full object-cover"
-                                />
-                            </div>
-                            <p className="text-dark-primary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
-                                Garden plants
-                            </p>
+                    {showPreviousDay.length === 0 ? (
+                        <div className="empty w-full h-full flex items-center justify-center">
+                            <img src={sunSVG} alt="sun" />
                         </div>
-                        <div className="detail flex gap-[3px] mr-[10px]">
-                            <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
-                            <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
-                        </div>
-                    </div>
+                    ) : (
+                        <>
+                            {showPreviousDay.map((task, index) => (
+                                <div className="task bg-yellow-meibo border-[2px] border-gray-title-secondary rounded-[15px] h-[45px] flex items-center justify-between px-[10px]">
+                                    <div className="left flex items-center">
+                                        <div className="w-7 h-7 rounded-full overflow-hidden mr-[10px]">
+                                            <img
+                                                src={ppKirby}
+                                                alt="Avatar"
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </div>
+                                        <p className="text-dark-primary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                            {task.projectName}
+                                        </p>
+                                    </div>
+                                    <div className="detail flex gap-[3px] mr-[10px]">
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                    </div>
+                                </div>
+                            ))}
 
-                    <div className="task bg-yellow-meibo border-[2px] border-gray-title-secondary rounded-[15px] h-[45px] flex items-center justify-between px-[10px]">
-                        <div className="left flex items-center">
-                            <div className="w-7 h-7 rounded-full overflow-hidden mr-[10px]">
-                                <img
-                                    src={ppKirby}
-                                    alt="Avatar"
-                                    className="w-full h-full object-cover"
-                                />
-                            </div>
-                            <p className="text-dark-primary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
-                                Garden plants
-                            </p>
-                        </div>
-                        <div className="detail flex gap-[3px] mr-[10px]">
-                            <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
-                            <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
-                        </div>
-                    </div>
-
-                    <div className="task h-[45px] flex items-center justify-center gap-[10px] px-[10px]">
-                        <p className="text-gray-title-secondary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
-                            Show more
-                        </p>{" "}
-                        <img src={moreSVG} alt="" />
-                    </div>
+                            {tasksPreviousDay.length > 3 && (
+                                <div className="showmore task h-[45px] flex items-center justify-center gap-[10px] px-[10px]">
+                                    <p className="text-gray-title-secondary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                        Show more
+                                    </p>
+                                    <img src={moreSVG} alt="" />
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
 
-                <div className="separator w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
+                <div className="separator min-w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
 
-                <div className="day active w-full px-[15px]">
-                    <h3 className="text-white-title text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px]">
+                <div className="day active w-full px-[10px] grid grid-rows-4 min-h-[120px]">
+                    <h3
+                        className={`text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px] ${
+                            activeDay.isSame(dayjs(), "day")
+                                ? "text-white-title"
+                                : "text-gray-title-secondary"
+                        }`}
+                    >
                         {activeDay.date()}
                     </h3>
-                    <div className="empty w-full h-full flex items-center justify-center">
-                        <img src={sunSVG} alt="sun" />
-                    </div>
+                    {showActiveDay.length === 0 ? (
+                        <div className="empty w-full h-full flex items-center justify-center">
+                            <img src={sunSVG} alt="sun" />
+                        </div>
+                    ) : (
+                        <>
+                            {showActiveDay.map((task, index) => (
+                                <div className="task bg-yellow-meibo border-[2px] border-gray-title-secondary rounded-[15px] h-[45px] flex items-center justify-between px-[10px]">
+                                    <div className="left flex items-center">
+                                        <div className="w-7 h-7 rounded-full overflow-hidden mr-[10px]">
+                                            <img
+                                                src={ppKirby}
+                                                alt="Avatar"
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </div>
+                                        <p className="text-dark-primary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                            {task.projectName}
+                                        </p>
+                                    </div>
+                                    <div className="detail flex gap-[3px] mr-[10px]">
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                    </div>
+                                </div>
+                            ))}
+
+                            {tasksActiveDay.length > 3 && (
+                                <div className="showmore task h-[45px] flex items-center justify-center gap-[10px] px-[10px]">
+                                    <p className="text-gray-title-secondary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                        Show more
+                                    </p>
+                                    <img src={moreSVG} alt="" />
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
 
-                <div className="separator w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
+                <div className="separator min-w-[3px] self-stretch bg-header-separation rounded-[3px]"></div>
 
-                <div
-                    className="day next w-full px-[15px]"
-                    onMouseEnter={() => setRightHover(true)}
-                    onMouseLeave={() => setRightHover(false)}
-                >
-                    <h3 className="text-gray-title-secondary text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px]">
-                        {rightHover ? (
-                            <div
-                                className="left-arrow flex h-[23px] w-[23px] m-auto justify-center items-center bg-dark-tertiary rounded-full hover:cursor-pointer"
-                                onClick={goRight}
-                            >
-                                <img src={rightSVG} alt="Left arrow" />
-                            </div>
-                        ) : (
-                            nextDay.date()
-                        )}
+                <div className="day next w-full px-[10px] grid grid-rows-4 min-h-[120px]">
+                    <h3
+                        className={`text-[23px] leading-[23px] font-semibold [letter-spacing:-0.05em] text-center mb-[15px] ${
+                            nextDay.isSame(dayjs(), "day")
+                                ? "text-white-title"
+                                : "text-gray-title-secondary"
+                        }`}
+                    >
+                        {nextDay.date()}
                     </h3>
+                    {showNextDay.length === 0 ? (
+                        <div className="empty w-full h-full flex items-center justify-center">
+                            <img src={sunSVG} alt="sun" />
+                        </div>
+                    ) : (
+                        <>
+                            {showNextDay.map((task, index) => (
+                                <div className="task bg-yellow-meibo border-[2px] border-gray-title-secondary rounded-[15px] h-[45px] flex items-center justify-between px-[10px]">
+                                    <div className="left flex items-center">
+                                        <div className="w-7 h-7 rounded-full overflow-hidden mr-[10px]">
+                                            <img
+                                                src={ppKirby}
+                                                alt="Avatar"
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </div>
+                                        <p className="text-dark-primary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                            {task.projectName}
+                                        </p>
+                                    </div>
+                                    <div className="detail flex gap-[3px] mr-[10px]">
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                        <div className="dot w-[6px] h-[6px] bg-dark-primary rounded-full"></div>
+                                    </div>
+                                </div>
+                            ))}
+
+                            {tasksNextDay.length > 3 && (
+                                <div className="showmore task h-[45px] flex items-center justify-center gap-[10px] px-[10px]">
+                                    <p className="text-gray-title-secondary text-[20px] leading-[20px] font-semibold [letter-spacing:-0.05em]">
+                                        Show more
+                                    </p>
+                                    <img src={moreSVG} alt="" />
+                                </div>
+                            )}
+                        </>
+                    )}
                 </div>
 
-                <div className="sepaseparator w-[3px] self-stretch bg-header-separation rounded-[3px]rator"></div>
+                <div className="sepaseparator min-w-[3px] self-stretch bg-header-separation rounded-[3px]rator"></div>
             </div>
         </div>
     );
--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -10,16 +10,12 @@ import TaskCard from "@/Components/TaskCard.jsx";
 
 export default function Dashboard() {
 
-    const { tasks, projects, creator, className } = usePage().props;
-
-    console.log("tasks", tasks);
-    console.log("creator", creator);
-    console.log("project", projects);
+    const { tasks, projects, creator, auth} = usePage().props;
 
     return (
         <AuthenticatedLayout>
             <div className="grid grid-cols-[1.6fr_1fr] grid-rows-[1fr_3fr] gap-[15px] h-[100vh] p-[15px] box-border">
-                <AgendaDashboard tasks={tasks} projects={projects} />
+                <AgendaDashboard tasks={tasks} projects={projects} auth={auth} />
                 <TasksDashboard tasks={tasks} />
                 <ProjectsDashboard projects={projects} />              
             </div>
