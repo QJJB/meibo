613d7b5abf19e8135f0a5c4a970fe49df88511c4
Bastien Morrier <bastien.morrier@gmail.com>
2025-05-13 10:22:41 +0200

    ajout de la colonne color

---
+++ database/migrations/2025_05_13_080544_add_color_to_projects_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->string('color')->nullable()->after('description'); // ou à un autre endroit
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropColumn('color');
+        });
+    }
+
+};
