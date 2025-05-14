3c44a037b7f205487dc5f877c1c2535e6edb503e
Ugo <j.erghioui@gmail.com>
2025-05-14 09:11:36 +0200

    Dragou and Dropou

--- resources/js/Components/Project/Tasks/Column.jsx
+++ resources/js/Components/Project/Tasks/Column.jsx
@@ -1,11 +1,17 @@
+import { useDroppable } from '@dnd-kit/core';
 import Card from './Card';
 
-function Column({title, tasks, projectId}) {
+function Column({ id, title, tasks, projectId}) {
+
+    const { setNodeRef } = useDroppable({
+        id: id,
+    });
+
     return (
         <div className="w-full">
             <h3 className="text-[18px] text-gray-title-secondary leading-[18px] font-semibold [letter-spacing:-0.05em] mb-[32px] px-[10px]">{title}</h3>
 
-            <div className={`${title.split(' ').join('')} w-full px-[6px] grid gap-[10px]`}>
+            <div ref={setNodeRef} className={`${title.split(' ').join('')} w-full px-[6px] grid gap-[10px]`}>
 
                 {/* Map ici */}
                 {tasks.map(task => (
--- resources/js/Pages/Project.jsx
+++ resources/js/Pages/Project.jsx
@@ -9,6 +9,7 @@ export default function Project() {
 
     return (
         <AuthenticatedLayout>
+            <Head title={`Project - ${projects.name}`} />
             <div className="grid grid-cols-[2fr_1fr] grid-rows-[1fr_3fr] gap-[15px] h-[100vh] p-[15px] box-border">
                 <Description
                     projects={projects}
